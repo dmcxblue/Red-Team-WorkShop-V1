245cf6a69f01da920923ecfeff4c3242ede908f0
David <41899653+dmcxblue@users.noreply.github.com>
2023-10-05 11:52:31 -0700

    Add files via upload

---
+++ Weaponization & Initial Access/Hello World.js
@@ -0,0 +1,13 @@
+// JavaScript code (JScript)
+
+var shell = new ActiveXObject("WScript.Shell");
+shell.Popup("Hello World!", 0, "Hello, World", 64);
+
+//JavaScript Code
+
+WScript.Echo("Hello World!");
+
+
+
+
+
